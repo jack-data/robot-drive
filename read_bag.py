772b53f7d1d0811a3e1ad224c465a1ac3a1298cc
jack-data <143542967+jack-data@users.noreply.github.com>
2023-08-29 23:42:54 +0100

    Update read_bag.py
    Noticed some magic values used in place of variables.

--- read_bag.py
+++ read_bag.py
@@ -16,6 +16,12 @@ ROSBAG_TOPICS = ["/infrareds", "/cmd_vel"]
 INFRAREDS_NUMPY_FILE_PATH = "/home/vboxuser/MMP/numpy_arrays/infrareds/"
 CMD_VEL_NUMPY_FILE_PATH = "/home/vboxuser/MMP/numpy_arrays/cmd_vel/"
 
+INFRAREDS_CSV_FINAL_PATH = "/home/vboxuser/MMP/catkin_ws/src/rosbag_files/final_csvs/combined_infrareds.csv"
+CMD_VEL_CSV_FINAL_PATH = "/home/vboxuser/MMP/catkin_ws/src/rosbag_files/final_csvs/combined_cmd_vel.csv"
+
+INFRAREDS_COLUMNS = [10,19,28,37,46,55,64]
+CMD_VEL_COLUMNS = [2,7]
+
 #Create a list of files from the directory (and not the sub-directories created by bagreaders)
 files = [file for file in listdir(ROSBAG_FILE_DIR) if (isfile(join(ROSBAG_FILE_DIR, file)) and file.endswith('.bag'))] 
 
@@ -46,16 +52,16 @@ combined_infrareds_csvs = pd.concat(infrareds_csvs)
 combined_cmd_vel_csvs = pd.concat(cmd_vel_csvs)
 
 #Save the dataframes as a csv file
-combined_infrareds_csvs.to_csv("/home/vboxuser/MMP/catkin_ws/src/rosbag_files/final_csvs/combined_infrareds.csv")
-combined_cmd_vel_csvs.to_csv("/home/vboxuser/MMP/catkin_ws/src/rosbag_files/final_csvs/combined_cmd_vel.csv")
+combined_infrareds_csvs.to_csv(INFRAREDS_CSV_FINAL_PATH, delimiter=',')
+combined_cmd_vel_csvs.to_csv(CMD_VEL_CSV_FINAL_PATH, delimiter=',')
 
 #Create a numpy array from the new concatenated csv 
-infrareds_np_array = genfromtxt("/home/vboxuser/MMP/catkin_ws/src/rosbag_files/final_csvs/combined_infrareds.csv", delimiter=',')
-cmd_vel_np_array = genfromtxt("/home/vboxuser/MMP/catkin_ws/src/rosbag_files/final_csvs/combined_cmd_vel.csv", delimiter=',')
+infrareds_np_array = genfromtxt(INFRAREDS_CSV_FINAL_PATH, delimiter=',')
+cmd_vel_np_array = genfromtxt(CMD_VEL_CSV_FINAL_PATH, delimiter=',')
 
 #Extract the columns we are interested in from the numpy array
-infrareds_np_array = infrareds_np_array[:,[10,19,28,37,46,55,64]]
-cmd_vel_np_array = cmd_vel_np_array[:,[2,7]]
+infrareds_np_array = infrareds_np_array[:,INFRAREDS_COLUMNS]
+cmd_vel_np_array = cmd_vel_np_array[:,CMD_VEL_COLUMNS]
 
 #Remove NaN values
 infrareds_np_array = infrareds_np_array[~np.isnan(infrareds_np_array).any(axis=1)]
